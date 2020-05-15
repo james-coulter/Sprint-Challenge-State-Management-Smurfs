a4937895eb6d345593ad8ad4b8e93ca9b9fcf634
Christian Coulter <95coulter@gmail.com>
2020-05-15 09:30:33 -0600

    Finished get request. Fetching original obj. Working on post next.

---
+++ smurfs/src/components/SmurfList.js
@@ -0,0 +1,23 @@
+import React, { useContext } from 'react';
+import SmurfsContext from '../contexts/SmurfsContext.js';
+
+function SmurfList() {
+    const { smurfs } = useContext(SmurfsContext)
+
+    return (
+        <div>
+            {smurfs.map(item => (
+                <div key={item.id}>
+
+                    <h1>Hi, my name is {item.name}.</h1>
+                    <h2>I am {item.age} years old.</h2>
+                    <h2>I am a whopping {item.height} tall.</h2>
+
+                </div>
+            ))}
+        </div>
+    )
+
+}
+
+export default SmurfList;
